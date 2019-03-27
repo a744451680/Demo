130c6dfd7863b76104f685e91d26019e5cddce92
hyl <15010247110@163.com>
2019-03-27 16:52:37 +0800

    [2019-03-27] 注释掉：mybatis plus 的性能优化配置

--- src/main/java/top/isyl/demo/config/MybatisPlusConfig.java
+++ src/main/java/top/isyl/demo/config/MybatisPlusConfig.java
@@ -26,14 +26,14 @@ public class MybatisPlusConfig {
      * plus 的性能优化(该插件只用于开发环境，不建议生产环境使用)
      * @return
      */
-    @Bean
+   /* @Bean
     public PerformanceInterceptor performanceInterceptor() {
         PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-        /*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*/
+        *//*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*//*
         performanceInterceptor.setMaxTime(2000);
-        /*<!--SQL是否格式化 默认false-->*/
+        *//*<!--SQL是否格式化 默认false-->*//*
         performanceInterceptor.setFormat(true);
         return performanceInterceptor;
-    }
+    }*/
 
 }
